fe4cb86776130b59fa41ea516a2e95f88b7b25ca
Aarón Riveiro <aaronriveiro@gmail.com>
2026-08-23 11:08:50 +0200

    fix: avoid crash formatting missing visibility as N/A

--- src/plugins/weather/weather.py
+++ src/plugins/weather/weather.py
@@ -380,11 +380,12 @@ class Weather(BasePlugin):
         })
 
         # Visibility
-        current_visibility = "N/A"
+        current_visibility = None
         visibility_hourly_times = hourly_data.get('time', [])
         visibility_values = hourly_data.get('visibility', [])
         visibility_conversion = 0.001  # m to km
         visibility_max = 10.  # km
+        at_max_visibility = False
         for i, time_str in enumerate(visibility_hourly_times):
             try:
                 if parse_open_meteo_dt(time_str, tz).hour == current_time.hour:
@@ -394,9 +395,12 @@ class Weather(BasePlugin):
             except ValueError:
                 logger.warning(f"Could not parse time string {time_str} for visibility.")
                 continue
-        visibility_str = f"{current_visibility:.1f}"
-        if at_max_visibility:
-            visibility_str = u"\u2265" + visibility_str
+        if current_visibility is None:
+            visibility_str = "N/A"
+        else:
+            visibility_str = f"{current_visibility:.1f}"
+            if at_max_visibility:
+                visibility_str = u"\u2265" + visibility_str
         data_points.append({
             "label": "Visibility",
             "measurement": visibility_str,
